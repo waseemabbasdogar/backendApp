839be18b450ea3d70c88f584e2cab967b3df5025
waseemabbasdogar <wassim3451@gmail.com>
2026-01-03 10:21:53 +0500

    user and video models are added

---
+++ src/models/user.model.js
@@ -0,0 +1,82 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true, 
+        lowercase: true,
+        unique: true,
+        trim: true,
+        index: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    avatar: {
+        type : String, // cloudinary url
+        required: true
+    },
+    coverImage: {
+        type: String
+    },
+    refreshToken: {
+        type: String
+    },
+    watchHistory : {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }
+}, {
+    timestamps: true
+})
+
+userSchema.pre("save", async function(){
+    if(!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        username: this.username,
+        fullName: this.fullName,
+        email: this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -1,8 +1,8 @@
 class apiResponse {
     constructor(statusCode, message="success", data){
         this.statusCode = statusCode,
-        this.message = message,
         this.data = data
+        this.message = message,
         this.success = statusCode < 400
     }
 }
